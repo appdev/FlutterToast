16009fc4ad41510fb50a3946c0a46302e7c97f8c
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2020-02-06 17:09:33 +0400

    using textStyle instead of textColor

--- lib/toast.dart
+++ lib/toast.dart
@@ -2,22 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Toast {
-  static final int LENGTH_SHORT = 1;
-  static final int LENGTH_LONG = 2;
-  static final int BOTTOM = 0;
-  static final int CENTER = 1;
-  static final int TOP = 2;
+  static final int lengthShort = 1;
+  static final int lengthLong = 2;
+  static final int bottom = 0;
+  static final int center = 1;
+  static final int top = 2;
 
   static void show(String msg, BuildContext context,
       {int duration = 1,
       int gravity = 0,
       Color backgroundColor = const Color(0xAA000000),
-      Color textColor = Colors.white,
+      textStyle = const TextStyle(fontSize: 15, color: Colors.white),
       double backgroundRadius = 20,
       Border border}) {
     ToastView.dismiss();
-    ToastView.createView(
-        msg, context, duration, gravity, backgroundColor, textColor, backgroundRadius, border);
+    ToastView.createView(msg, context, duration, gravity, backgroundColor,
+        textStyle, backgroundRadius, border);
   }
 }
 
@@ -34,8 +34,15 @@ class ToastView {
   static OverlayEntry _overlayEntry;
   static bool _isVisible = false;
 
-  static void createView(String msg, BuildContext context, int duration, int gravity,
-      Color background, Color textColor, double backgroundRadius, Border border) async {
+  static void createView(
+      String msg,
+      BuildContext context,
+      int duration,
+      int gravity,
+      Color background,
+      TextStyle textStyle,
+      double backgroundRadius,
+      Border border) async {
     overlayState = Overlay.of(context);
 
     Paint paint = Paint();
@@ -57,15 +64,15 @@ class ToastView {
                   ),
                   margin: EdgeInsets.symmetric(horizontal: 20),
                   padding: EdgeInsets.fromLTRB(16, 10, 16, 10),
-                  child:
-                      Text(msg, softWrap: true, style: TextStyle(fontSize: 15, color: textColor)),
+                  child: Text(msg, softWrap: true, style: textStyle),
                 )),
           ),
           gravity: gravity),
     );
     _isVisible = true;
     overlayState.insert(_overlayEntry);
-    await new Future.delayed(Duration(seconds: duration == null ? Toast.LENGTH_SHORT : duration));
+    await new Future.delayed(
+        Duration(seconds: duration == null ? Toast.lengthShort : duration));
     dismiss();
   }
 
